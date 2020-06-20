296973bc1f1ebce7503d43764cf1b38c55e877b0
dev!caelo <58437950+dev-caelo@users.noreply.github.com>
2020-06-20 15:19:44 -0700

    finished js and info display onclick

--- classInfo.js
+++ classInfo.js
@@ -19,19 +19,44 @@ individual lists: every list variable should have a variable name as such:
 
 info<classAcronym>
 
-- each of these variables MUST include their respective acronym at
+- classKEY: each of these variables MUST include their respective acronym at
   the first element of this list [this is for easier accessing on the website's end]
   < also bc idk how to make this better oops >
 
-- these lists can be as long as required to hold all of the info
+- length: these lists can be as long as required to hold all of the info
+
+- url links: these must be embedded into the string with an HTML <a> tag to function
+	
+	ex: <a href='[insert full https link here]'>[shortened url]</a>
+
 */
 
+/* EXAMPLE:
+
+	const info<CLASSNAME> = [
+	"classKEY", 
+	"classNAME",
+	"<a href='https://www.youtube.com'>YouTube</a>",
+	];
+
+*/
 
 
 //individual lists: 
 
-const infoCSA = ["CSA", "<a href='https://www.codingbat.com/java'>codingbat.com</a>", "quizlet"];
-const infoCSP = ["CSP", "<a href='https://www.codingbat.com/python'>codingbat.com</a>", "canvas"];
+const infoCSA = [
+	"csa", 
+	"CSA",
+	"<a href='https://www.codingbat.com/java'>- Java Practice - codingbat.com</a>", 
+	"<a href='https://youtu.be/RRubcjpTkks'>- Alex Lee - Learn Java in 14 minutes (seriously)</a>",
+	];
+
+const infoCSP = [
+	"csp", 
+	"CSP",
+	"<a href='https://www.codingbat.com/python'>- Python Practice - codingbat.com</a>", 
+	"<a href='https://quizlet.com/207034674/ap-computer-science-principles-exam-quizlet-flash-cards/'>- Quizlet - CS Terminology</a>",
+	];
 
 
 //classList: UPDATE the list with each class variable added
--- testJSList.js
+++ testJSList.js
@@ -7,6 +7,8 @@ appears on the screen.
 
 //EDIT: STICK TO BUTTONS, ITS EASIER AND LOOKS BETTER
 
+
+//calls info of a class, given by classKey, formats the info, and outputs the info onto the screen
 function fillInfo(classKey) {
 	var myList = getClass(String(classKey));
 
@@ -16,14 +18,16 @@ function fillInfo(classKey) {
 	if(myList != undefined) {
 		var text = document.getElementById(myList[0]).innerHTML;
 		for(var i = 1; i < myList.length; i++) {
-			text += myList[i] + "<br>";
+			text += myList[i] + "</br>";
 		}		
-		if(document.getElementById(myList[0]).innerHTML.localeCompare("") == 0) {
+		
+		if(document.getElementById(myList[0]).innerHTML.localeCompare("") == 0) { //will only display if not displayed already (prevent dupes)
 			document.getElementById(myList[0]).innerHTML = text;
 		}
 	}
 }
 
+//calls the class requested through comparisons with classKey - returns the classList requested, or undefined if classList not found 
 function getClass(classKey) {
 	for(var i = 0; i < classList.length; i++) {
 		if(classList[i][0].localeCompare(classKey) == 0) {
@@ -31,15 +35,38 @@ function getClass(classKey) {
 		}
 	}
 
-	return null;
+	return undefined;
 }
 
-const idList = ["info", "CSA", "CSP"]; //change
+//takes a class' information and formats it for output in fillInfo()
+function displayInfo(classList) {
+	var classHead = "<h1>" + classList[0] + "</h1>";
+}
 
 function clr() {
-	for(var i = 0; i < idList.length; i++) {
-		document.getElementById(idList[i]).innerHTML = "";
+	for(var i = 0; i < classList.length; i++) {
+		document.getElementById(classList[i][0]).innerHTML = "";
 	}
 }
 
-const test = "Test";
+function show(ID, buttonID) {
+	var element = document.getElementById(String(ID));
+	element.style.display = "block";
+
+	var button = document.getElementById(String(buttonID));
+	button.onclick = function() {
+		hide(ID, buttonID);
+	};
+	button.style.backgroundColor = "#5a5a57";
+}
+
+function hide(ID, buttonID) {
+	var element = document.getElementById(String(ID));
+	element.style.display = "none";
+
+	var button = document.getElementById(String(buttonID));
+	button.onclick = function() {
+		show(ID, buttonID);
+	};
+	button.style.backgroundColor = "#393939";
+}
